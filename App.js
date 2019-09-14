670874d5d75d2a06e33dbbcce6358513d542deee
Ricky Dam <RickyDamOttawa@gmail.com>
2019-09-14 19:00:52 -0400

    Use double quotes for consistency

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import AppNavigator from './AppNavigator';
+import AppNavigator from "./AppNavigator";
 
 export default class App extends React.Component {
     constructor(props) {
