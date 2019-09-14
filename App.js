d2c3ff785a898ab18ee1f3afa0be6b04d73d7830
Ricky Dam <RickyDamOttawa@gmail.com>
2019-09-14 19:02:54 -0400

    Remove unnecessary spacing

--- App.js
+++ App.js
@@ -12,15 +12,9 @@ export default class App extends React.Component {
 
     addFriend = (index) => {
         const {currentFriends, possibleFriends} = this.state;
-
-        // Pull friend out of possibleFriends
-        const addedFriend = possibleFriends.splice(index, 1);
-
-        // And put friend in currentFriends
-        currentFriends.push(addedFriend);
-
-        // Finally, update our app state
-        this.setState({currentFriends, possibleFriends});
+        const addedFriend = possibleFriends.splice(index, 1); // Pull friend out of possibleFriends
+        currentFriends.push(addedFriend); // And put friend in currentFriends
+        this.setState({currentFriends, possibleFriends}); // Finally, update our app state
     };
 
     render() {
