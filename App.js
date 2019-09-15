a3f1d93310091108946675389371ab6765e8e775
Ricky Dam <RickyDamOttawa@gmail.com>
2019-09-15 15:03:41 -0400

    Remove hardcoded state array values in App.js

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            possibleFriends: ['Husky', 'Chihuahua', 'Beagle'],
+            possibleFriends: [],
             currentFriends: []
         };
     }
