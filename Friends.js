30a8a96538c16aa322b3d92f702d35fbcc22beed
Ricky Dam <RickyDamOttawa@gmail.com>
2019-09-15 01:42:20 -0400

    Add Redux mapStateToProps function to Friends screen

--- Friends.js
+++ Friends.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {StyleSheet, Text, View, Button} from "react-native";
+import {connect} from "react-redux";
 
-export default class Friends extends React.Component {
+class Friends extends React.Component {
     render() {
         return (
             <View style={styles.container}>
                 <Text>Add friends here!</Text>
                 {
-                    this.props.screenProps.possibleFriends.map((friend, index) => (
+                    this.props.friends.possible.map((friend, index) => (
                         <Button
                             key={friend}
                             title={`Add ${friend}`}
@@ -32,3 +33,10 @@ const styles = StyleSheet.create({
         justifyContent: "center"
     }
 });
+
+const mapStateToProps = (state) => {
+    const {friends} = state;
+    return {friends};
+};
+
+export default connect(mapStateToProps)(Friends);
